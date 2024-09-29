c86573b3bd8c51035a6d3ad1005eac460e7a6a47
Trithinh11 <lytrithinh2011@gmail.com>
2024-09-29 17:41:22 +0700

    lesson1
    person and account

---
+++ Lesson/Lesson1.py
@@ -0,0 +1,40 @@
+class Person:
+    def __init__(self, _cP, _aP, fn, nn, bt, rl, ed):
+        #ham khoi tao-> khi minh tao ra mot doi tuong moi
+        self.coverpicture = _cP
+        self.avatarPicture = _aP
+        self.fullname = fn
+        self.nickname = nn
+        self.birthday = bt
+        self.relationship = rl
+        self.education = ed
+    #ham de ghi ra thong tin cua nguoi do
+    def show(self):
+         print(self.__dict__)
+    #ham luu thong tin vao file
+    def saveDataToFile(self):
+        #mo file duoi dang viet
+        with open("person.txt", "w") as file:
+            data = ""
+            data += self.coverpicture +","
+            data += self.avatarPicture +","
+            data += self.fullname +","
+            data += self.nickname +","
+            data += self.birthday +","
+            data += self.relationship +","
+            data += self.education +""
+            #ghi file vao
+            file.write(data)
+    #ham doc thong tin tu file
+    def loadDataFromFile(self):
+        #mo file duoi dang hoc
+        with open("person.txt", "r") as file:
+            #doc het tat ca cac dong
+            data = (file.readline())
+            #chia du lieu thanh tung thuoc tinh
+            self.coverpicture, self.avatarPicture, self.fullname, self.nickname, self.birthday, self.relationship, self.education = data.split(",")
+            
+
+ps1 = Person("", "", "", "", "", "", "")
+ps1.loadDataFromFile()
+ps1.show()
---
+++ model/account.py
@@ -0,0 +1,41 @@
+class Account:
+    def __init__(self, _un = "Tai Khoan", _pw = "123"):
+       self.username = _un
+       self.password = _pw
+    #    self.loadDataFromFile()
+    #ham in ra thong tin tai khoa
+    def show(self):
+         print(self.__dict__)
+        #ham luu thong tin tu file
+    def saveDataToFile(self):
+        #mo file duoi dang viet
+        with open("Data/Account.txt", "w") as file:
+            data = ""
+            data += self.username +","
+            data += self.password 
+            #ghi file vao
+            file.write(data)
+    #ham doc thong tin tu file
+    def loadDataFromFile(self):
+        #mo file duoi dang hoc
+        with open("Data/Account.txt", "r") as file:
+            #doc het tat ca cac dong
+            data = str(file.readline())
+            #chia du lieu thanh tung thuoc tinh
+            self.username, self.password  = data.split(",")
+    #ham doi mat khau
+    def changePassword(self,newPassword):
+        self.password = newPassword 
+        self.saveDataToFile()
+    #ham kiem tra dang nhap
+    def checkAccount(self,_un, _pw):
+        if _un == self.username and _pw == self.password:
+            print("Dung thong tin")
+        else:
+            print("Sai thong tin") 
+
+
+ac1 = Account()
+ac1. saveDataToFile()
+ac1. changePassword("123454321")
+ac1. checkAccount("taikhoan", "1234554321")
